5de4170689cc9c9bc8ba681b7578601597895715
Preshh <uniquep201@gmail.com>
2020-05-20 00:47:16 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 function validate(){
-    var x = document.forms["myForm"] [document.getElementById("email")].value [document.getElementById("username")].value [document.getElementsByClassName("checkbox")].value ;
+    var x = document.forms["myForm"];
+    var y =  document.getElementById("username").value; 
+    var z = document.getElementById("email").value;
+    var checkbox = document.getElementsByClassName("checkbox").value ;
 
     if (x == null || x == ""){
         alert("Incomplete fields must be filled out");
+        return false;
+    }else if (y == null || y == ""){
+        alert ("username needs to be filled");
+        return false;
+    }else if (z == null || z == ""){
+        alert ("email needs to be completed");
+        return false;
+    }else if (checkbox == null || checkbox == ""){
+        alert ("checkbox needs to be clicked");
         return false;
     }
 }
